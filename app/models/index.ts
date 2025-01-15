af7eb553d7bfd644528892f10bbc750c5478bd99
Wilfred Chen <wilfred.chen.python@gmail.com>
2025-01-16 07:38:57 +1100

    Add PromptVersion to index file

--- app/models/index.ts
+++ app/models/index.ts
@@ -6,3 +6,4 @@ export { default as AnalysisQueue } from './AnalysisQueue';
 export { default as EmailLog } from './EmailLog';
 export { default as UserTag } from './UserTag';
 export { default as ApiLog } from './ApiLog'; 
+export { default as PromptVersion } from './PromptVersion'; 
