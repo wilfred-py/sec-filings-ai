b31819e3820186fb934c77054732df4dcb6e9e30
Wilfred Chen <wilfred.chen.python@gmail.com>
2025-01-15 21:33:56 +1100

    Create Index file for all models

---
+++ app/models/index.ts
@@ -0,0 +1,8 @@
+export { default as User } from './User';
+export { default as Watchlist } from './Watchlist';
+export { default as Security } from './Security';
+export { default as FilingCache } from './FilingCache';
+export { default as AnalysisQueue } from './AnalysisQueue';
+export { default as EmailLog } from './EmailLog';
+export { default as UserTag } from './UserTag';
+export { default as ApiLog } from './ApiLog'; 
