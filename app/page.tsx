99ef3d0cdd221e29e8267f1137fbae6a8c97cf9f
Wilfred Chen <wilfred.chen.python@gmail.com>
2025-01-28 16:11:53 +1100

    Update import of jsdonld on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import { Input } from "@/components/ui/input"
 import { Bell, Sun, Moon, Mail } from 'lucide-react'
 import { LoadingButton } from '@/components/ui/loadingButton'
+import JsonLd from '@/components/JsonLd'
 
 import { notifications, transactionRelatedFilings, ownershipChanges } from '@/components/exampleSummaries'
 import GrowthIndicator from '@/components/GrowthIndicator'
@@ -122,13 +123,11 @@ const jsonLd = {
   "@type": "SoftwareApplication",
   "name": "tldrSEC",
   "applicationCategory": "FinanceApplication",
-  "description": "AI-powered SEC filings summary service",
-  "aggregateRating": {
-    "@type": "AggregateRating",
-    "ratingValue": "",
-    "ratingCount": "",
-    "bestRating": "5",
-    "worstRating": "1"
+  "description": "AI-powered SEC filings summary service that delivers insights straight to your inbox",
+  "offers": {
+    "@type": "Offer",
+    "price": "159",
+    "priceCurrency": "USD"
   },
   // Requirements
   "requirements": "Modern web browser with JavaScript enabled",
@@ -455,8 +454,7 @@ return (
     </div>
 </div>
 
-
-
+<JsonLd data={jsonLd} />
 
 {/* 
 <a 
