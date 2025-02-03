a086b90b58bc6a44ee24af2965b4d33f722f30ab
Wilfred Chen <wilfred.chen.python@gmail.com>
2025-02-03 17:15:44 +1100

    Create redis file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "./auth";
 
-export const { GET, POST } = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
