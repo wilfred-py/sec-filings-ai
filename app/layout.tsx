7b5a8c20db36d8e055100a8d7514b46933f18616
Wilfred Chen <wilfred.chen.python@gmail.com>
2025-02-17 12:34:29 +1100

    Update auth with more detailed error logging and create debug-providers route

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,11 +78,11 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-        <Providers>
-          {children}
-          <Analytics />
-          <SpeedInsights />
-        </Providers>
+        {/* <Providers> */}
+        {children}
+        <Analytics />
+        <SpeedInsights />
+        {/* </Providers> */}
       </body>
     </html>
   );
